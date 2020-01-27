f64f14865a71e2d84ed6feb7fe7d55385b747f3a
Luis Herres <luis@herres.dev>
2020-01-27 22:33:47 +0100

    Updates

---
+++ precache-manifest.6a41b5e426621453adaf47f720c28034.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "899b77522e660b5abe6ab3df6360c903",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "c83bf166937ffb54a13e",
+    "url": "/admin/static/css/2.25b83785.chunk.css"
+  },
+  {
+    "revision": "4ba114b0060d6b994232",
+    "url": "/admin/static/css/main.da005ed0.chunk.css"
+  },
+  {
+    "revision": "c83bf166937ffb54a13e",
+    "url": "/admin/static/js/2.057371e4.chunk.js"
+  },
+  {
+    "revision": "4ba114b0060d6b994232",
+    "url": "/admin/static/js/main.36708dc6.chunk.js"
+  },
+  {
+    "revision": "2fc9fce03fbacc7c7161",
+    "url": "/admin/static/js/runtime~main.72e8751d.js"
+  },
+  {
+    "revision": "028b8c73cb47c61e35061d82efb00ed6",
+    "url": "/admin/static/media/BringBack.028b8c73.svg"
+  },
+  {
+    "revision": "9b615e92903b6f915ea5e2901f1ee148",
+    "url": "/admin/static/media/PickUp.9b615e92.svg"
+  },
+  {
+    "revision": "37f2e0703796fae803efa698f661919d",
+    "url": "/admin/static/media/admin.37f2e070.svg"
+  }
+]);
